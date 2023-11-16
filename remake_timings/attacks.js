253f076eaa8111ca7d743a7d41b27b7e1113e30a
pidgezero <pidgezero.one@gmail.com>
2023-11-15 21:04:37 -0500

    script test

--- remake_timings/attacks.js
+++ remake_timings/attacks.js
@@ -10,5 +10,6 @@ var attacks = {
     info: "Right when the tongue makes contact",
     note: "",
     frames: 6,
-  }
+  },
+  "Mallow Shocker": {type: "attack", perfect: '00:00:02.049', partialStart: '00:00:01.983', partialEnd: '00:00:02.416', video: './resources/mallow-shocker.mp4', poster: './resources/mallow-shocker.png', name: 'Mallow Shocker', info: 'Right when lightning strikes', note: '', frames: 5})
 };
